bd3b85778c5e91a29379dc24050d41268a7124e2
Ali Sattarzadeh <alisattarzadeh71@gmail.com>
2023-05-23 02:42:04 +0330

    adding 404 page

--- src/pages/500.jsx
+++ src/pages/500.jsx
@@ -3,15 +3,11 @@ import serverErrorImage from "~/src/assets/images/serverError.svg"
 function Error({ statusCode }) {
     return (
         <div className="w-full h-[80%] relative">
-            <Image src={serverErrorImage} alt="server error icon" />
-            {statusCode && `An error ${statusCode} occurred on server`}
+            <Image src={serverErrorImage} fill alt="server error icon" />
+            An error occurred on server
         </div>
     );
 }
 
-Error.getInitialProps = ({ res, err }) => {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-    return { statusCode };
-};
 
 export default Error;
--- src/pages/country/[cca2].tsx
+++ src/pages/country/[cca2].tsx
@@ -4,28 +4,28 @@ import qs from "qs";
 import Country from "~/src/Utils/Entities/Country";
 
 interface ICountryDetailProps{
-    country:Country
+    country?:Country
 }
-const CountryDetail:NextPage<ICountryDetailProps> = ({country}) => {
-    console.log({country})
+const CountryDetail:NextPage<ICountryDetailProps> = () => {
+    // console.log({country})
     return (<div></div>)
 }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-
-    const countries = await CountryController.getAllCountries(qs.stringify({fields: 'cca2'}));
-
-    return {
-        paths:countries.map(c=>({params:{cca2:c.cca2}})),
-        fallback: true,
-    };
-};
-
-export const getStaticProps: GetStaticProps<{
-    country: Country;
-}> = async ({params}:any) => {
-    const countries = await CountryController.getCountryDetail(params.cca2);
-    return { props: { country:countries[0] } };
-};
+// export const getStaticPaths: GetStaticPaths = async () => {
+//
+//     const countries = await CountryController.getAllCountries(qs.stringify({fields: 'cca2'}));
+//
+//     return {
+//         paths:countries.map(c=>({params:{cca2:c.cca2}})),
+//         fallback: true,
+//     };
+// };
+//
+// export const getStaticProps: GetStaticProps<{
+//     country: Country;
+// }> = async ({params}:any) => {
+//     const countries = await CountryController.getCountryDetail(params.cca2);
+//     return { props: { country:countries[0] } };
+// };
 
 export default CountryDetail
